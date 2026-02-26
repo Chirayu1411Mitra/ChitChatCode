ad14e0b445b737a03405e163d816be78752e8a80
Gagan499 <syangagandeep2305@gmail.com>
2026-02-26 14:30:48 +0530

    update the port number

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { loadEnv } from 'vite'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: parseInt(loadEnv('', path.resolve(__dirname, '..'), '').VITE_FRONTEND_PORT || '5173'),
+  },
 })
